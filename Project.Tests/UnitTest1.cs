deccebf9ef28d3c927f6a257919a6bebc93c029e
npadula <nicolasvpadula@gmail.com>
2018-05-02 10:36:33 -0300

    Agrega tests

---
+++ Project.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace Project.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(1, 21);
+        }
+    }
+}
