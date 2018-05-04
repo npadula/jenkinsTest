4cb2208cf5f94ed941caafeee5ff98f507bb6461
npadula <nicolasvpadula@gmail.com>
2018-05-04 10:59:11 -0300

    Back to TRX test log

--- buildscripts/run_tests.ps1
+++ buildscripts/run_tests.ps1
@@ -1,6 +1,7 @@
 $DeployDir = "C:\Users\nicolas.padula\Desktop\JenkinsTestDeploy"
 
 Write-Output "Running tests..."
+#test
 
 dotnet publish -c Release 
 dotnet vstest Project.Tests\bin\Release\netcoreapp2.0\Project.Tests.dll /logger:trx 
