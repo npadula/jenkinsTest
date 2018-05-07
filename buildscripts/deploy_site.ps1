3e5cadb90cc2026161e02bf6ad28db9aa9180865
npadula <nicolasvpadula@gmail.com>
2018-05-07 09:41:06 -0300

    test

--- buildscripts/deploy_site.ps1
+++ buildscripts/deploy_site.ps1
@@ -1,5 +1,6 @@
 $DeployDir = "C:\Users\nicolas.padula\Desktop\JenkinsTestDeploy"
-
+$currentDir = Convert-Path .
+$buildDir = "$($currentDir)\build"
 
 Write-Output "Stopping website..."
 Import-Module WebAdministration
@@ -7,3 +8,6 @@ Stop-Website 'JenkinsTestDeploy'
 
 Write-Output "Removing previous files from deploy directory..."
 Remove-Item -path $DeployDir -Recurse
+
+Write-Output "Deploying build to $($DeployDir)..."
+Copy-Item $buildDir -Destination $DeployDir -Recurse
--- buildscripts/post_always.ps1
+++ buildscripts/post_always.ps1
@@ -1,3 +1,7 @@
 $testFilesDir = "TestResults"
+$buildDir = "build"
 Write-Output "Removing used test files..."
 Remove-Item -path $testFilesDir -Recurse
+
+Write-Output "Removing build directory..."
+Remove-Item -path $buildDir -Recurse
