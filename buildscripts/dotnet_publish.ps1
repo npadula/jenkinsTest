8f6bc1a56db3bc276385dbec7c4c65e06c9989b6
npadula <nicolasvpadula@gmail.com>
2018-05-07 09:21:37 -0300

    test

--- buildscripts/dotnet_publish.ps1
+++ buildscripts/dotnet_publish.ps1
@@ -1,6 +1,8 @@
 $DeployDir = "C:\Users\nicolas.padula\Desktop\JenkinsTestDeploy"
+$currentDir = Convert-Path .
+$buildDir = "$($currentDir)\build"
 
 
 Write-Output "Running dotnet publish..."
-dotnet publish -c Release -o $DeployDir
-cd $DeployDir
+dotnet publish -c Release -o $buildDir
+#cd $DeployDir
--- buildscripts/run_tests.ps1
+++ buildscripts/run_tests.ps1
@@ -1,8 +1,11 @@
 $DeployDir = "C:\Users\nicolas.padula\Desktop\JenkinsTestDeploy"
+$currentDir = Convert-Path .
+$buildDir = "$($currentDir)\build"
 
 Write-Output "Running tests..."
 #testtest
 
-dotnet publish -c Release 
+#dotnet publish -c Release 
+
 dotnet vstest Project.Tests\bin\Release\netcoreapp2.0\Project.Tests.dll /logger:trx 
 
