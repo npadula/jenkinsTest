c115d12dcd0e7ec1133a19f11805c7b07db48588
npadula <nicolasvpadula@gmail.com>
2018-06-04 15:27:33 -0300

    Test

--- buildscripts/dotnet_publish.ps1
+++ buildscripts/dotnet_publish.ps1
@@ -6,6 +6,7 @@ $csProjFile = "$($currentDir)\JenkinsTst\JenkinsTest.csproj"
 
 Write-Output "Running dotnet publish..."
 dotnet publish -c Release #Build everything within the solution, necessary for building test project
+Write-Output "$($csProjFile)"
 dotnet publish $csProjFile -c Release -o $buildDir #Publish only "root" project, necessary for deploying to IIS later
 #dotnet publish -c Release -o $buildDir
 
